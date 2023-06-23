9855a53eb82e12fbb3d6e994a1bfbfd0695b2af2
silva4dev <web4dev.lucas@outlook.com>
2023-06-23 15:30:55 -0300

    initial

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CoursesModule } from './courses/courses.module';
+
+@Module({
+  imports: [
+    CoursesModule,
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'docker',
+    //   database: 'postgres',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
